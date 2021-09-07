9ebaeabc38cb404bd9e6ea422bea6ecbf2894d57
hyeon-aa <ahhyun1120@naver.com>
2021-09-08 01:49:30 +0900

    feat(Comment)

--- src/modules/comment/comment.controller.ts
+++ src/modules/comment/comment.controller.ts
@@ -1,4 +1,41 @@
-import { Controller } from '@nestjs/common';
+import {
+    Body,
+    Controller,
+    Delete,
+    Get,
+    Param,
+    Post,
+    Put,
+    Query
+  } from '@nestjs/common';
+import { CreateCommentDto } from './dto/create-comment.dto';
+import { CommentService } from './comment.service';
+import { UpdateCommentDto } from './dto/update-comment.dto';
+import { Comment } from 'src/entities/comment.entity';
+  
+  @Controller('comment')
+  export class CommentController {
+    constructor(private readonly CommentService: CommentService) {}
+  
+    @Post()
+    async createComment(@Body() dto: CreateCommentDto) {
+      return this.CommentService.createComment(dto);
+    }
 
-@Controller('comment')
-export class CommentController {}
+    @Get()
+    async getAll(@Query('postId') postId: number) {
+      return this.CommentService.getAll(postId);
+    }
+  
+    @Put(':id')
+    async updateComment(@Param('id') id: number, @Body() dto: UpdateCommentDto) {
+      return this.CommentService.findAndUpdate(id, dto);
+    }
+  
+    @Delete(':id')
+    async deleteComment(@Param('id') id: number) {
+      return this.CommentService.deleteCommentById(id);
+    }
+  }
+  
+
--- src/modules/comment/comment.service.ts
+++ src/modules/comment/comment.service.ts
@@ -1,4 +1,60 @@
-import { Injectable } from '@nestjs/common';
+import { Comment } from 'src/entities/comment.entity';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateCommentDto } from './dto/create-comment.dto';
+import { UpdateCommentDto } from './dto/update-comment.dto';
+import { Post } from 'src/entities/post.entity';
+import { User } from 'src/entities/user.entity';
 
 @Injectable()
-export class CommentService {}
+export class CommentService {
+  constructor(
+    @InjectRepository(Comment)
+    private CommentRepository: Repository<Comment>,
+    @InjectRepository(Post)
+    private PostRepository: Repository<Post>,
+    @InjectRepository(User)
+    private UserRepository: Repository<User>,
+  ) {}
+
+  async createComment( commentData : CreateCommentDto): Promise<Comment> {
+    const {postId,userId,content} = commentData;
+    const post = await this.PostRepository.findOne(postId)
+    if (post == null ){
+      throw new NotFoundException();
+    }
+    const user = await this.UserRepository.findOne(userId);
+    if (user === undefined) {
+      throw new NotFoundException();
+    }
+    const comment = this.CommentRepository.create( {post ,user, content});
+    return await this.CommentRepository.save(comment);
+  }
+
+  async getAll(postId): Promise<Comment[]> {
+    const comments = await this.CommentRepository.find(postId);
+    return comments;
+  }
+
+  async findOne(id: number): Promise<Comment> {
+    const comment = await this.CommentRepository.findOne(id);
+    if (comment == null) {
+      throw new NotFoundException();
+    }
+    return comment;
+  }
+
+  async findAndUpdate( id : number, {content} : UpdateCommentDto): Promise<Comment> {
+    const comment = await this.findOne(id);
+    comment.content = content;
+    return await this.CommentRepository.save(comment);
+  }
+
+  async deleteCommentById(id: number): Promise<void> {
+    await this.CommentRepository.delete(id);
+    return;
+  }
+}
+
+
