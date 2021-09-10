c696986272ebac7c73927eb0db1ee4d76080ec0e
hyeon-aa <ahhyun1120@naver.com>
2021-09-10 23:27:37 +0900

    feat(comment2)

--- src/modules/comment/comment.controller.ts
+++ src/modules/comment/comment.controller.ts
@@ -1,41 +1,39 @@
 import {
-    Body,
-    Controller,
-    Delete,
-    Get,
-    Param,
-    Post,
-    Put,
-    Query
-  } from '@nestjs/common';
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
 import { CreateCommentDto } from './dto/create-comment.dto';
 import { CommentService } from './comment.service';
 import { UpdateCommentDto } from './dto/update-comment.dto';
 import { Comment } from 'src/entities/comment.entity';
-  
-  @Controller('comment')
-  export class CommentController {
-    constructor(private readonly CommentService: CommentService) {}
-  
-    @Post()
-    async createComment(@Body() dto: CreateCommentDto) {
-      return this.CommentService.createComment(dto);
-    }
 
-    @Get()
-    async getAll(@Query('postId') postId: number) {
-      return this.CommentService.getAll(postId);
-    }
-  
-    @Put(':id')
-    async updateComment(@Param('id') id: number, @Body() dto: UpdateCommentDto) {
-      return this.CommentService.findAndUpdate(id, dto);
-    }
-  
-    @Delete(':id')
-    async deleteComment(@Param('id') id: number) {
-      return this.CommentService.deleteCommentById(id);
-    }
+@Controller('comment')
+export class CommentController {
+  constructor(private readonly CommentService: CommentService) {}
+
+  @Post()
+  async createComment(@Body() dto: CreateCommentDto) {
+    return this.CommentService.createComment(dto);
   }
-  
 
+  @Get()
+  async getAll(@Query('postId') postId: number) {
+    return this.CommentService.getAll(postId);
+  }
+
+  @Put(':id')
+  async updateComment(@Param('id') id: number, @Body() dto: UpdateCommentDto) {
+    return this.CommentService.findAndUpdate(id, dto);
+  }
+
+  @Delete(':id')
+  async deleteComment(@Param('id') id: number) {
+    return this.CommentService.deleteCommentById(id);
+  }
+}
--- src/modules/comment/comment.module.ts
+++ src/modules/comment/comment.module.ts
@@ -3,9 +3,11 @@ import { CommentController } from './comment.controller';
 import { CommentService } from './comment.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Comment } from 'src/entities/comment.entity';
+import { Post } from 'src/entities/post.entity';
+import { User } from 'src/entities/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Comment])],
+  imports: [TypeOrmModule.forFeature([Comment, Post, User])],
   controllers: [CommentController],
   providers: [CommentService]
 })
