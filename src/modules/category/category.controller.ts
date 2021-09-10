8ff2c390668c8a6d3dc6976ddf8b10013c95f991
Lee Sohyeong <69043819+sohyeong-dev@users.noreply.github.com>
2021-09-11 01:14:42 +0900

    feat: category CRUD (#6)
    Co-authored-by: dlthgud <so-hyeong@naver.com>

--- src/modules/category/category.controller.ts
+++ src/modules/category/category.controller.ts
@@ -1,4 +1,47 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { CategoryService } from './category.service';
+import { CreateCategoryDto } from './dto/create-category.dto';
+import { UpdateCategoryDto } from './dto/update-category.dto';
 
 @Controller('category')
-export class CategoryController {}
+export class CategoryController {
+  constructor(private readonly categoryService: CategoryService) {}
+
+  @Get()
+  async getAllCategories() {
+    return this.categoryService.findAll();
+  }
+
+  @Get(':id')
+  async getCategory(@Param('id', ParseIntPipe) id: number) {
+    return this.categoryService.findOne(id);
+  }
+
+  @Post()
+  async addCategory(@Body() data: CreateCategoryDto) {
+    const categoryId = await this.categoryService.create(data);
+    return { categoryId };
+  }
+
+  @Patch(':id')
+  async updateName(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() dto: UpdateCategoryDto,
+  ) {
+    return this.categoryService.updateName(id, dto.name);
+  }
+
+  @Delete(':id')
+  async deleteCategory(@Param('id', ParseIntPipe) id: number) {
+    return this.categoryService.deleteById(id);
+  }
+}
