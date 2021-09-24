a7fc446586b589bebabff3db908321f0c6c17f28
Lee Sohyeong <69043819+sohyeong-dev@users.noreply.github.com>
2021-09-24 21:14:50 +0900

    feat: tag CRUD (#8)
    Co-authored-by: dlthgud <so-hyeong@naver.com>

---
+++ src/modules/tag/dto/update-tag.dto.ts
@@ -0,0 +1,3 @@
+import { TagDto } from './create-tag.dto';
+
+export class UpdateTagDto extends TagDto {}
--- src/modules/tag/tag.controller.ts
+++ src/modules/tag/tag.controller.ts
@@ -1,4 +1,46 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { TagDto } from './dto/create-tag.dto';
+import { UpdateTagDto } from './dto/update-tag.dto';
+import { TagService } from './tag.service';
 
 @Controller('tag')
-export class TagController {}
+export class TagController {
+  constructor(private readonly tagService: TagService) {}
+
+  @Get()
+  async getAllTags() {
+    return this.tagService.findAll();
+  }
+
+  @Get(':id')
+  async getTag(@Param('id', ParseIntPipe) id: number) {
+    return this.tagService.findById(id);
+  }
+
+  @Post()
+  async addTag(@Body() data: TagDto) {
+    return this.tagService.create(data);
+  }
+
+  @Patch(':id')
+  async updateTag(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() dto: UpdateTagDto,
+  ) {
+    return this.tagService.findAndUpdate(id, dto);
+  }
+
+  @Delete(':id')
+  async deleteTag(@Param('id', ParseIntPipe) id: number) {
+    return this.tagService.deleteById(id);
+  }
+}
